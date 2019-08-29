0f64a6b8320d0e9421e3f88c023b1cb34ede48d5
Brodhead <abrodhead@zagmail.gonzaga.edu>
2019-08-29 10:10:49 -0700

    added another newline

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String args[]) {
-        System.out.println("My second hello world!\n");
+        System.out.println("My second hello world!\n\n");
 
     }
 }
